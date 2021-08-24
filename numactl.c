numactl/numactl

commit 498385e3aaf265d6e9786e0a391196cd82ab3260

numactl.c: fix use after free
The following command can trigger the bug
  numactl --length 65536 --shm xxx -p0 -V > /dev/null

So reset mask to block any new access inside this loop.

Signed-off-by: Pingfan Liu <piliu@redhat.com>

numactl.c
@@ -544,6 +544,7 @@ int main(int ac, char **av)
 			if (nnodes != 1)
 				usage();
 			numa_bitmask_free(mask);
+			mask = NULL;
 			errno = 0;
 			did_node_cpu_parse = 1;
 			numa_set_bind_policy(0);

commit 59772a28ab02cdf814ac6a33ebcdd69604e5a12e

numactl.c: fixed debug verify for --preferred option
numactl argument --verify return "Need a mask to verify" when
memory police uses --preferred option.

fixes: 498385e3aaf2 ("numactl.c: fix use after free")

numactl.c
@@ -543,8 +543,6 @@ int main(int ac, char **av)
 			}
 			if (nnodes != 1)
 				usage();
-			numa_bitmask_free(mask);
-			mask = NULL;
 			errno = 0;
 			did_node_cpu_parse = 1;
 			numa_set_bind_policy(0);