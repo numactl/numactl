numactl/numactl

commit d538c6453618f3dd6445056a9a343f52fdb23c10

libnuma: Query upcoming MPOL_PREFERRED_MANY support
This patch attempts to improve the experience for users who wish to use
new numactl with an old kernel. It helps autodetect whether or not the
new preferred interface is there and then can dynamically enable/disable
usage internally.

Doing this allows it to be used by the v1.1 interface.

Signed-off-by: Ben Widawsky <ben.widawsky@intel.com>
Signed-off-by: Feng Tang <feng.tang@intel.com>

libnuma.c
@@ -27,6 +27,7 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <ctype.h>
+#include <assert.h>
 
 #include <sys/mman.h>
 #include <limits.h>
@@ -78,6 +79,8 @@ static int numprocnode = -1;
 static int numproccpu = -1;
 static int nodemask_sz = 0;
 static int cpumask_sz = 0;
+
+static int has_preferred_many = 0;
 
 int numa_exit_on_error = 0;
 int numa_exit_on_warn = 0;
@@ -589,6 +592,30 @@ set_configured_cpus(void)
 		numa_error("sysconf(NPROCESSORS_CONF) failed");
 }
 
+static void
+set_kernel_abi()
+{
+	int oldp;
+	struct bitmask *bmp, *tmp;
+	bmp = numa_allocate_nodemask();
+	tmp = numa_allocate_nodemask();
+
+	if (get_mempolicy(&oldp, bmp->maskp, bmp->size + 1, 0, 0) < 0)
+		goto out;
+
+	/* Assumes there's always a node 0, and it's online */
+	numa_bitmask_setbit(tmp, 0);
+	if (set_mempolicy(MPOL_PREFERRED_MANY, tmp->maskp, tmp->size) == 0) {
+		has_preferred_many++;
+		/* reset the old memory policy */
+		setpol(oldp, bmp);
+	}
+
+out:
+	numa_bitmask_free(tmp);
+	numa_bitmask_free(bmp);
+}
+
 /*
  * Initialize all the sizes.
  */
@@ -601,6 +628,7 @@ set_sizes(void)
 	set_numa_max_cpu();	/* size of kernel cpumask_t */
 	set_configured_cpus();	/* cpus listed in /sys/devices/system/cpu */
 	set_task_constraints(); /* cpus and nodes for current task */
+	set_kernel_abi();	/* man policy supported */
 }
 
 int
@@ -1804,6 +1832,11 @@ void numa_set_preferred(int node)
 	numa_bitmask_setbit(bmp, node);
 	__numa_set_preferred(bmp);
 	numa_bitmask_free(bmp);
+}
+
+int numa_has_preferred_many(void)
+{
+	return has_preferred_many;
 }
 
 void numa_set_localalloc(void)

numa.h
@@ -186,6 +186,9 @@ static inline void numa_free_nodemask(struct bitmask *b)
 /* Some node to preferably allocate memory from for task. */
 void numa_set_preferred(int node);
 
+/* Returns whether or not the platform supports MPOL_PREFERRED_MANY */
+int numa_has_preferred_many(void);
+
 /* Set local memory allocation policy for task */
 void numa_set_localalloc(void);
 

numaif.h
@@ -27,7 +27,8 @@ extern long move_pages(int pid, unsigned long count,
 #define MPOL_BIND        2
 #define MPOL_INTERLEAVE  3
 #define MPOL_LOCAL       4
-#define MPOL_MAX         5
+#define MPOL_PREFERRED_MANY   5
+#define MPOL_MAX         6
 
 /* Flags for set_mempolicy, specified in mode */
 #define MPOL_F_NUMA_BALANCING	(1 << 13) /* Optimize with NUMA balancing if possible */

versions.ldscript
@@ -154,3 +154,11 @@ libnuma_1.5 {
   local:
     *;
 } libnuma_1.4;
+
+
+libnuma_1.6{
+  global:
+    numa_has_preferred_many;
+  local:
+    *;
+} libnuma_1.5;

commit a5742b18991e182146ad9f4d3f79cd84a7ad099d

libnuma: clear errno at the end of numa_init
The construct function numa_init may change errno value
before main function, which can be confuse.

Fixes: d538c645("libnuma: Query upcoming MPOL_PREFERRED_MANY support")

libnuma.c
@@ -106,6 +106,9 @@ numa_init(void)
         for (i = 0; i < max; i++)
                 nodemask_set_compat((nodemask_t *)&numa_all_nodes, i);
 	memset(&numa_no_nodes, 0, sizeof(numa_no_nodes));
+
+	/* clear errno */
+	errno = 0;
 }
 
 static void cleanup_node_cpu_mask_v2(void);