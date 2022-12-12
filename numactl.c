numactl/numactl

commit 5862e0e45d2c3f5cec64ec993d3744299f0f6bc9

numactl: Simplify preferred selection
This will help consolidate preferred and preferred-many usage.

Signed-off-by: Ben Widawsky <ben.widawsky@intel.com>
Signed-off-by: Feng Tang <feng.tang@intel.com>

numactl.c
@@ -420,7 +420,7 @@ static struct bitmask *numactl_parse_nodestring(char *s, int flag)
 
 int main(int ac, char **av)
 {
-	int c, i, nnodes=0;
+	int c;
 	long node=-1;
 	char *end;
 	char shortopts[array_len(opts)*2 + 1];
@@ -535,13 +535,7 @@ int main(int ac, char **av)
 				printf ("<%s> is invalid\n", optarg);
 				usage();
 			}
-			for (i=0; i<mask->size; i++) {
-				if (numa_bitmask_isbitset(mask, i)) {
-					node = i;
-					nnodes++;
-				}
-			}
-			if (nnodes != 1)
+			if (numa_bitmask_weight(mask) != 1)
 				usage();
 			errno = 0;
 			did_node_cpu_parse = 1;
@@ -549,7 +543,8 @@ int main(int ac, char **av)
 			if (shmfd >= 0)
 				numa_tonode_memory(shmptr, shmlen, node);
 			else
-				numa_set_preferred(node);
+				numa_set_preferred(find_first(mask));
+			numa_bitmask_free(mask);
 			checkerror("setting preferred node");
 			break;
 		case 'l': /* --local */

commit dda3b9b5c3ed5fbe7b11c5b2b4db3ec153bd499e

numactl: 'numactl --length=xxx --shm xxx -px' doesn't work
When preferred is used for shm, no value is assigned to the variable node,
As a result, it does not take effect.

fixes: 5862e0e4("numactl: Simplify preferred selection")

numactl.c
@@ -548,7 +548,7 @@ int main(int ac, char **av)
 			did_node_cpu_parse = 1;
 			numa_set_bind_policy(0);
 			if (shmfd >= 0) {
-				numa_tonode_memory(shmptr, shmlen, node);
+				numa_tonode_memory(shmptr, shmlen, find_first(mask));
 				/* Correspond to numa_set_bind_policy function */
 				if (numa_has_preferred_many()) {
 					setpolicy(MPOL_PREFERRED_MANY);