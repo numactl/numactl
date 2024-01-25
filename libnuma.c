numactl/numactl

commit 87c683467f7578714e495c52f04cc40f5f8b757b

libnuma: Convert preferred node into a mask
This helps converge code for supporting upcoming MPOL_PREFERRED_MANY
interface.

Signed-off-by: Ben Widawsky <ben.widawsky@intel.com>
Signed-off-by: Feng Tang <feng.tang@intel.com>

libnuma.c
@@ -1765,39 +1765,44 @@ out:
 	return ret;
 }
 
-int numa_preferred(void)
+static struct bitmask *__numa_preferred(void)
 {
 	int policy;
 	struct bitmask *bmp;
-	/* could read the current CPU from /proc/self/status. Probably
-	   not worth it. */
-	int ret = 0;
 
 	bmp = numa_allocate_nodemask();
+	/* could read the current CPU from /proc/self/status. Probably
+	   not worth it. */
+	numa_bitmask_clearall(bmp);
 	getpol(&policy, bmp);
 
 	if (policy != MPOL_PREFERRED && policy != MPOL_BIND)
-		return ret;
+		return bmp;
 
-	int i;
-	int max = numa_num_possible_nodes();
-	for (i = 0; i < max ; i++)
-		if (numa_bitmask_isbitset(bmp, i))
-			ret = i;
+	if (numa_bitmask_weight(bmp) > 1)
+		numa_error(__FILE__);
 
-	return ret;
+	return bmp;
+}
+
+int numa_preferred(void)
+{
+	return find_first(__numa_preferred());
+}
+
+static void __numa_set_preferred(struct bitmask *bmp)
+{
+	int nodes = numa_bitmask_weight(bmp);
+	if (nodes > 1)
+		numa_error(__FILE__);
+	setpol(nodes ? MPOL_PREFERRED : MPOL_LOCAL, bmp);
 }
 
 void numa_set_preferred(int node)
 {
-	struct bitmask *bmp;
-
-	bmp = numa_allocate_nodemask();
-	if (node >= 0) {
-		numa_bitmask_setbit(bmp, node);
-		setpol(MPOL_PREFERRED, bmp);
-	} else
-		setpol(MPOL_LOCAL, bmp);
+	struct bitmask *bmp = numa_allocate_nodemask();
+	numa_bitmask_setbit(bmp, node);
+	__numa_set_preferred(bmp);
 	numa_bitmask_free(bmp);
 }
 

commit a4b134795c156be921043a7cd6b9f738cbaab620

libnuma: Fix incorrect print and exit of numa_preferred/_many APIs
Only when policy is preferred, numa_bitmask_weight will be judged,
because mbind and preferred_many support multiple nodes.

fixes: 87c6834("libnuma: Convert preferred node into a mask")

Signed-off-by: Chunsheng Luo <luochunsheng@ustc.edu>

libnuma.c
@@ -1882,7 +1882,7 @@ static struct bitmask *__numa_preferred(void)
 			policy != MPOL_BIND)
 		return bmp;
 
-	if (numa_bitmask_weight(bmp) > 1) {
+	if (policy == MPOL_PREFERRED && numa_bitmask_weight(bmp) > 1) {
 		errno = EINVAL;
 		numa_error(__FILE__);
 	}